c13ef4a1f060fd92b13142a6e388c1557e955059
gekashatalov <geka.shatalov@gmail.com>
2021-05-11 22:42:12 +0300

    Верстка блока с карточка и блока Материалы

--- private_html/js/site.visual.js
+++ private_html/js/site.visual.js
@@ -1,9 +1,15 @@
 $(document).ready(function (){
     burgerBtnToggler();
+    productSizeSelectric();
 });
+//
 function burgerBtnToggler(){
     let button = $('.header-menu-burger');
     button.on('click', function(){
         $(this).find('.header-menu-btn').toggleClass('active');
     });
+}
+//initial custom select
+function productSizeSelectric () {
+	$('select.product-size').selectric();
 }
