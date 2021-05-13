c7ce2e65befac02ebd71807943a67867254a774f
gekashatalov <geka.shatalov@gmail.com>
2021-05-13 22:31:08 +0300

    верстка слайдера и секции отзывы

--- private_html/js/site.visual.js
+++ private_html/js/site.visual.js
@@ -1,15 +1,36 @@
 $(document).ready(function (){
-    burgerBtnToggler();
-    productSizeSelectric();
+	burgerBtnToggler();
+	productSizeSelectric();
+	swiperSlider();
 });
 //
 function burgerBtnToggler(){
-    let button = $('.header-menu-burger');
-    button.on('click', function(){
-        $(this).find('.header-menu-btn').toggleClass('active');
-    });
+	let button = $('.header-menu-burger');
+	button.on('click', function(){
+		$(this).find('.header-menu-btn').toggleClass('active');
+	});
 }
 //initial custom select
 function productSizeSelectric () {
 	$('select.product-size').selectric();
+}
+//initial swiper-slider
+function swiperSlider(){
+	const swiper = new Swiper('.slider-container', {
+		slidesPerView:'auto',
+		speed: 400,
+		spaceBetween: 8,
+		watchSlidesVisibility : true,
+		breakpoints: {
+			320 : {
+				spaceBetween: 8,
+			},
+			641 : {
+				spaceBetween: 16,
+			},
+			1025: {
+				spaceBetween: 45,
+			}
+		}
+	});
 }
